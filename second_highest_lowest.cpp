b39da8f4601018736b25a403d860c5e83ea241b4
tranvankhuyen <khuyendvdtht@gmail.com>
2021-08-06 11:16:38 +0700

    New file

---
+++ second_highest_lowest.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+// Copy Array function..
+void Copy_Array(int a[], int b[], int size_a)
+{
+    for(int i = 0; i <= size_a - 1; i++)
+    {
+        a[i] = b[i];
+    }
+}
+// Delete element function..
+void Delete_element(int a[], int size_a, int x)
+{
+    for(int i = x; i <= size_a - 1; i++)
+    {
+        a[i] = a[i + 1];
+        a[size_a] = '\0'; 
+    }
+}
+// Sort function : Max -> min.
+void Sort(int a[], int n)
+{
+    for(int i = 0; i <= n -2; i++)
+    {
+        for(int j = i+1; j <= n - 1; j++)
+        {
+            if(a[i] < a[j])
+            {
+                std::swap(a[i], a[j]);
+            }
+        }
+    }
+}
+// Remove redundant elements function.
+void Remove_redundant_elements(int a[], int &n)
+{
+    for(int i = 0; i <= n - 1; i++)
+    {
+        for(int j = i+1; j <= n - 1; j++)
+        {
+            if(a[i] == a[j])
+            {
+                Delete_element(a, n, j);
+                n--;
+            }
+        }
+    }
+}
+//main function
+int main(){
+    int a[] = {1,5,4,4,5};
+    int size_t = sizeof(a)/sizeof(a[0]);
+    Sort(a, size_t);
+    Remove_redundant_elements(a, size_t);
+    if(size_t <= 2)
+    {
+        std::cout<<"INVALID";
+    }
+    else
+    {
+        std::cout<<a[1]<<" "<<a[size_t - 2];
+    }
+    return 0;
+}
